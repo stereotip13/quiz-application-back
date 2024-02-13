ec0edf4a9976533f4b84bae0aaadbc75b40e9bab
Маяковский Александр Александрович <MayakovskyAA@ufk53.internet.local>
2024-02-13 18:06:54 +0500

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import Sequelize from './db';
 import { ConfigService } from '@nestjs/config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 //require('dotenv').config();
