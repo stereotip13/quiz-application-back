9b565bcc04fe66ec1461bcb32c82f2312f1eecc9
jsenjoyer <jsenjoyer@icloud.com>
2024-03-23 07:15:29 +0300

    401 fix

--- src/guards/jwt-guard.ts
+++ src/guards/jwt-guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  //you have to create canActivate method to return true or false depending of any checks you need
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return true;
+  }
+}
