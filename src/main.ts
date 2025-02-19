4f39668b3c2199425272a4914fabf85c1d2445ee
stereotip13 <stereotip13@gmail.com>
2025-02-19 17:13:05 +0500

    change cors

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,31 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const PORT = configService.get('port');
+    // app.enableCors({
+    //   origin: 'https://localhost:5173', // Указывает, с какого origin разрешены запросы
+    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    //   allowedHeaders: 'Content-Type, Authorization',
+    // });
     app.enableCors({
-      origin: 'http://localhost:5173', // Указывает, с какого origin разрешены запросы
+      origin: (origin, callback) => {
+        // Разрешаем запросы без Origin (например, cURL или Postman)
+        if (!origin) {
+          return callback(null, true);
+        }
+
+        // Проверяем, начинается ли origin с http://192.168.1.
+        // Или разрешаем localhost:5173 (при необходимости)
+        const isLocalNetwork = /^http:\/\/192\.168\.1\.\d{1,3}(:\d+)?$/.test(
+          origin,
+        );
+        const isLocalhost5173 = origin === 'http://localhost:5173';
+
+        if (isLocalNetwork || isLocalhost5173) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: 'Content-Type, Authorization',
     });
